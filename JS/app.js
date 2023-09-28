7a32c4c47883ec5b072ce7741e98eed8eecbf4e6
Romain Sonnet <romain.sonnet2003@gmail.com>
2023-09-28 22:31:53 +0200

    commencement de la partie home et horizontal scroll

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,28 @@
-console.log("Hello World !!!");
+gsap.registerPlugin(ScrollTrigger);
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Animation du texte dans la partie home
+    const animatedText = document.querySelectorAll(".animated-text");
+    animatedText.forEach((element) => {
+        const rect = element.getBoundingClientRect();
+        if (rect.top < window.innerHeight) {
+            element.classList.add("active");
+        }
+    });
+
+    const menus = gsap.utils.toArray(".obj-menu");
+    menus.forEach((menu, index) => {
+        gsap.from(menu, {
+            opacity: 0,
+            y: 100,
+            scale: 0.8,
+            duration: 1,
+            scrollTrigger: {
+                trigger: ".home",
+                start: "top top",
+                end: "center top",
+                scrub: true,
+            },
+        });
+    });
+});
